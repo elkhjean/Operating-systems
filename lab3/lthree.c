88b578feec934188aa9d55cbd7474cbe03350b55
elkhjean <sean.elkhoury@gmail.com>
2023-11-28 10:28:22 +0100

    initial lab3

---
+++ lab3/lthree.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+char testnumbers[] = {1, 256, 32768, 32769, 128, 65534, 33153};
+
+int main(int argc, char const *argv[])
+{
+    int len = sizeof(testnumbers);
+
+    for (size_t i = 0; i < len; i++)
+    {
+        testnumbers[i]
+    }
+    
+    
+    return 0;
+}
